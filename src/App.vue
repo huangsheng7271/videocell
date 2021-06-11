cd8af39c066f4399c83927ca3bcd7c3e3e4c787c
huangsheng <1490085442@qq.com>
2021-06-11 16:28:37 +0800

    测试ant design vue的响应式

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,32 @@
 <template>
-  <div id="app" class="app-wapper">
+  <!-- <div id="app" class="app-wapper">
     <div class="button-wapper">
       <a-button type="primary" shape="round" size="large" class="button-shadow" style="margin-top:5rem">
         立即登陆
       </a-button>
     </div>
+  </div> -->
+
+  <div class="gutter-example">
+    <a-row :gutter="[16, { xs: 8, sm: 16, md: 24, lg: 32 }]">
+      <a-col
+        :xs="0" :sm="4" :md="6" :lg="8" :xl="10"
+        class="gutter-row" :span="6">
+        <div class="gutter-box">col-6</div>
+      </a-col>
+      <a-col 
+        :xs="24" :sm="16" :md="12" :lg="8" :xl="4"
+        class="gutter-row" :span="6">
+        <div class="gutter-box">col-6</div>
+      </a-col>
+      <a-col
+        :xs="0" :sm="4" :md="6" :lg="8" :xl="10"
+        class="gutter-row" :span="6">
+        <div class="gutter-box">col-6</div>
+      </a-col>
+    </a-row>
   </div>
+
 </template>
 
 <script>
@@ -29,5 +50,14 @@ export default {
 
 .ant-btn{
   box-shadow:0 2px 4px rgb(0 0 0 / 8%);
+}
+
+.gutter-example :deep(.ant-row > div) {
+  background: transparent;
+  border: 0;
+}
+.gutter-box {
+  background: #00a0e9;
+  padding: 5px 0;
 }
 </style>
