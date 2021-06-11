e94e35febfa5937c662b798d7f78c9103f814627
huangsheng <huangsheng>
2021-06-11 09:47:32 +0800

    自定义 ant design 主题

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,33 @@
 <template>
-  <a-button type="primary" shape="round" size="large" class="button-style">立即登陆</a-button>
+  <div id="app">
+     <a-button type="primary" shape="round" size="large" class="button-style">立即登陆</a-button>
+     <br><br>
+     <a-button type="primary" size="large" class="button-style">立即注册</a-button>
+  </div>
 </template>
 
 <script>
 export default {
+  //组件名
   name: 'App',
+  //子组件
   components: {
     
-  }
+  },
+  //数据
+  data: () => ({
+    
+  }),
 }
 </script>
 
-<style>
-  .button-style{
-    margin-top: 10rem;
-    margin-left: 10rem;
-  }
+<style scoped>
+.button-style{
+  margin-top: 10rem; 
+  box-shadow: 0 3px 8px -2px rgb(85 85 85 / 10%), 0 2px 12px 0 rgb(85 85 85 / 8%), 0 1px 12px 0 rgb(85 85 85 / 4%);
+}
+
+#app{
+  text-align: center;
+}
 </style>
